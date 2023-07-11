db19c6dc374e4223d5fee3cddc7f4908c8193519
GautamMarkande <118103083+GautamMarkande@users.noreply.github.com>
2023-07-11 20:25:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 function fibonacci(num) {
 // your code here
+	let prev=0, prev2=1;
+	let curr = 0;
+	for(let i = 2; i<=num; i++){
+		curr = prev + prev2;
+		prev = prev2;
+		prev2 = curr;
+	}
+	//console.log(prev2);
+	return prev2;
 }
 
 module.exports = fibonacci;
